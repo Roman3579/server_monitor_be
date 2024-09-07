799809d30b5014b5aeb2c98e41bd3cc9ba7f1d6e
RomanGlvac <romanglvac@gmail.com>
2024-09-07 14:48:24 +0200

    create StorageConfig

---
+++ src/main/java/com/unionutilities/unionutilities/config/StorageConfig.java
@@ -0,0 +1,15 @@
+package com.unionutilities.unionutilities.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties("config.storage")
+public class StorageConfig {
+    private String folderPath;
+    private String resultFilePath;
+}
