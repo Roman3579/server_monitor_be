cf5e8f15dc44925f505d217e0672a4abf404227b
RomanGlvac <romanglvac@gmail.com>
2024-09-07 16:10:41 +0200

    add setup runner

--- src/main/java/com/unionutilities/unionutilities/config/AppConfig.java
+++ src/main/java/com/unionutilities/unionutilities/config/AppConfig.java
@@ -1,12 +1,23 @@
 package com.unionutilities.unionutilities.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.unionutilities.unionutilities.service.FileService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import okhttp3.OkHttpClient;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
+
 @Configuration
-public class AppConfig {
+@AllArgsConstructor
+@Slf4j
+public class AppConfig implements ApplicationRunner {
+
+    private final FileService fileService;
 
     @Bean
     public ObjectMapper objectMapper() {
@@ -16,6 +27,30 @@ public class AppConfig {
     @Bean
     public OkHttpClient okHttpClient() {
         return new OkHttpClient();
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        setupApp();
+    }
+
+    private void setupApp() throws IOException {
+        setupStorage();
+    }
+
+    private void setupStorage() throws IOException {
+        log.info("Setting up storage.");
+        if(!fileService.createStorageFolder()){
+            log.info("Storage directory not created, it probably already exists.");
+        } else {
+            log.info("Storage directory created.");
+        }
+        if(!fileService.createResultsFile()){
+            log.info("Storage file not created, it probably already exists.");
+        } else {
+            log.info("Storage file created.");
+        }
+        log.info("Storage setup finished.");
     }
 
 }
