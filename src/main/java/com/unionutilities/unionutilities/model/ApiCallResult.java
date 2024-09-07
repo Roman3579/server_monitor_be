0b891d862fdd8125347aafef46101b5e55acb74e
RomanGlvac <romanglvac@gmail.com>
2024-09-07 15:14:16 +0200

    add no args constructor to ApiCallResult and ApiCallResultContainer

--- src/main/java/com/unionutilities/unionutilities/model/ApiCallResult.java
+++ src/main/java/com/unionutilities/unionutilities/model/ApiCallResult.java
@@ -2,9 +2,11 @@ package com.unionutilities.unionutilities.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ApiCallResult {
     private String targetUrl;
     private AppInfoModel appInfoModel;
--- src/main/java/com/unionutilities/unionutilities/model/ApiCallResultContainer.java
+++ src/main/java/com/unionutilities/unionutilities/model/ApiCallResultContainer.java
@@ -2,11 +2,13 @@ package com.unionutilities.unionutilities.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ApiCallResultContainer {
     private List<ApiCallResult> apiCallResults;
 }
