97efd1df8e6e3b0d56f2cecb7e5fe970a12296bb
RomanGlvac <romanglvac@gmail.com>
2024-09-07 15:34:16 +0200

    added refresh endpoint

--- src/main/java/com/unionutilities/unionutilities/web/AppInfoCollectionController.java
+++ src/main/java/com/unionutilities/unionutilities/web/AppInfoCollectionController.java
@@ -1,6 +1,7 @@
 package com.unionutilities.unionutilities.web;
 
 import com.unionutilities.unionutilities.model.ApiCallResultContainer;
+import com.unionutilities.unionutilities.service.AppInfoService;
 import com.unionutilities.unionutilities.service.FileService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,17 +10,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/results")
 @AllArgsConstructor
 public class AppInfoCollectionController {
 
+    private final AppInfoService appInfoService;
     private final FileService fileService;
 
     @GetMapping
     public ResponseEntity<ApiCallResultContainer> getAllApiCallResults() throws IOException {
         return ResponseEntity.ok().body(fileService.getStoredResults());
+    }
+
+    @GetMapping("/refresh")
+    public ResponseEntity<ApiCallResultContainer> refreshAllApiCallResults() {
+        appInfoService.refreshAppInfos();
+        Optional<ApiCallResultContainer> containerOptional = appInfoService.getStoredInfos();
+        if(containerOptional.isPresent()){
+            return ResponseEntity.ok().body(containerOptional.get());
+        } else {
+            throw new NoSuchElementException();
+        }
     }
 
 }
