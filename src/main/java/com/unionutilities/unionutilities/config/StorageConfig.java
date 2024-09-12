8198f6e63576f733657cb6ac5b01b0d86f37433a
glvac <roman.glvac@union.sk>
2024-09-12 14:14:29 +0200

    implement safeguard for unset storage attributes

--- src/main/java/com/unionutilities/unionutilities/config/StorageConfig.java
+++ src/main/java/com/unionutilities/unionutilities/config/StorageConfig.java
@@ -2,6 +2,7 @@ package com.unionutilities.unionutilities.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +10,21 @@ import org.springframework.stereotype.Component;
 @Getter
 @Setter
 @ConfigurationProperties("config.storage")
-public class StorageConfig {
+public class StorageConfig implements InitializingBean {
     private String folderPath;
     private String resultFilePath;
+
+    @Override
+    public void afterPropertiesSet() {
+        if (!isPropertySet(this.folderPath)){
+            throw new IllegalArgumentException("Storage folder path (config.storage.folder-path) not set.");
+        }
+        if (!isPropertySet(this.resultFilePath)){
+            throw new IllegalArgumentException("Result file name/path (config.storage.result-file-path) not set");
+        }
+    }
+
+    private Boolean isPropertySet(String propertyValue){
+        return propertyValue != null && !propertyValue.trim().isEmpty();
+    }
 }
