8d73875a19e7cae44bb98a7bd44dced93a50a9d2
glvac <roman.glvac@union.sk>
2024-09-17 11:44:19 +0200

    create LogRequest model

---
+++ src/main/java/com/unionutilities/unionutilities/model/request/LogRequest.java
@@ -0,0 +1,10 @@
+package com.unionutilities.unionutilities.model.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class LogRequest {
+    private String ip;
+}
