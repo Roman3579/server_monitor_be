4989063dda320fdb41b40814d7c9f1af4ef3a5c1
RomanGlvac <romanglvac@gmail.com>
2024-09-21 10:13:18 +0200

    create method for ApiCallError deserialization

--- src/main/java/com/unionutilities/unionutilities/client/ApiCallError.java
+++ src/main/java/com/unionutilities/unionutilities/client/ApiCallError.java
@@ -1,10 +1,13 @@
 package com.unionutilities.unionutilities.client;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
-@ToString
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ApiCallError {
 
     NOT_FOUND(404, "Not found."),
@@ -17,5 +20,20 @@ public enum ApiCallError {
     ApiCallError(Integer errorCode, String errorDescription) {
         this.errorCode = errorCode;
         this.errorDescription = errorDescription;
+    }
+
+    @JsonCreator
+    public static ApiCallError forValues(
+            @JsonProperty("errorCode") Integer errorCode,
+            @JsonProperty("errorDescription") String errorDescription) {
+        if(errorCode == null || errorDescription == null){
+            return null;
+        }
+        for (ApiCallError error : ApiCallError.values()) {
+            if (error.errorCode.equals(errorCode) && error.errorDescription.equals(errorDescription)){
+                return error;
+            }
+        }
+        return null;
     }
 }
