1a8e083523566bfa847ccd147b16ac9b5d4d3208
RomanGlvac <romanglvac@gmail.com>
2024-09-27 17:20:17 +0200

    fix FileConfig

--- src/main/java/com/unionutilities/unionutilities/config/storage/FileConfig.java
+++ src/main/java/com/unionutilities/unionutilities/config/storage/FileConfig.java
@@ -1,13 +1,11 @@
 package com.unionutilities.unionutilities.config.storage;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
-@AllArgsConstructor
 @ConfigurationProperties("config.files")
 public class FileConfig {
     private String resultFilePath;
