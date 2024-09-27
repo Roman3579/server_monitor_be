707675d7dbec83f4b20f520bdc9c1f19ed0242cf
RomanGlvac <romanglvac@gmail.com>
2024-09-27 18:55:04 +0200

    add endpoint to retrieve api call targets

--- src/main/java/com/unionutilities/unionutilities/web/AppSettingsController.java
+++ src/main/java/com/unionutilities/unionutilities/web/AppSettingsController.java
@@ -5,12 +5,10 @@ import com.unionutilities.unionutilities.web.requests.AddTargetRequest;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.Set;
 
 @Slf4j
 @RestController
@@ -28,6 +26,11 @@ public class AppSettingsController {
         } catch (IOException e) {
             return ResponseEntity.internalServerError().body(false);
         }
+    }
+
+    @GetMapping("/target")
+    public ResponseEntity<Set<String>> getTargets() {
+        return ResponseEntity.ok().body(appSettingService.getTargets());
     }
 
 }
