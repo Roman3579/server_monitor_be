ca6fb48f4e2500e72073fb8b485d9f0e383694a3
RomanGlvac <romanglvac@gmail.com>
2024-09-27 16:19:41 +0200

    create FileService interface

---
+++ src/main/java/com/unionutilities/unionutilities/service/files/FileService.java
@@ -0,0 +1,7 @@
+package com.unionutilities.unionutilities.service.files;
+
+import java.io.IOException;
+
+public interface FileService {
+    Boolean setupBaseFileOrDirectory() throws IOException;
+}
